d31d3a64fa9acf97dda36ba808e0e81c2b92468b
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2024-01-08 17:21:45 -0300

    FEAT: validation with formz

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -43,76 +43,39 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatefulWidget {
+class _RegisterForm extends StatelessWidget {
   const _RegisterForm();
-
-  @override
-  State<_RegisterForm> createState() => _RegisterFormState();
-}
-
-class _RegisterFormState extends State<_RegisterForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     final registerCubit = context.watch<RegisterCubit>();
+    final username = registerCubit.state.username;
+    final password = registerCubit.state.password;
+    final email = registerCubit.state.email;
+    //
+
     return Form(
-      key: _formKey,
       child: Column(children: [
         CustomTextFormField(
-          label: 'Nombre de usuario',
-          onChanged: (value) {
-            registerCubit.usernameChanged(value);
-            _formKey.currentState?.validate();
-          },
-          validator: (value) {
-            if (value == null || value.isEmpty) return 'Campo requerido';
-            if (value.trim().isEmpty) return 'Campo requerido';
-            if (value.length < 6) {
-              return 'El campo debe tener mas de 6 caracteres';
-            }
-            return null;
-          },
-        ),
+            label: 'Nombre de usuario',
+            onChanged: registerCubit.usernameChanged,
+            errorMessage: username.errorMessage),
         const SizedBox(height: 10),
         CustomTextFormField(
           label: 'Correo Electronico',
-          onChanged: (value) {
-            registerCubit.emailChanged(value);
-            _formKey.currentState?.validate();
-          },
-          validator: (value) {
-            if (value == null || value.isEmpty) return 'Campo requerido';
-            if (value.trim().isEmpty) return 'Campo requerido';
-            final emailRegExp = RegExp(
-              r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-            );
-            if (!emailRegExp.hasMatch(value)) 'Correo electronico no valido';
-            return null;
-          },
+          onChanged: registerCubit.emailChanged,
+          errorMessage: email.errorMessage,
         ),
         const SizedBox(height: 10),
         CustomTextFormField(
           label: 'Contraseña',
           obscureText: true,
-          onChanged: (value) {
-            registerCubit.passwordChanged(value);
-            _formKey.currentState?.validate();
-          },
-          validator: (value) {
-            if (value == null || value.isEmpty) return 'Campo requerido';
-            if (value.trim().isEmpty) return 'Campo requerido';
-            if (value.length < 6) {
-              return 'El campo debe tener mas de 6 caracteres';
-            }
-            return null;
-          },
+          onChanged: registerCubit.passwordChanged,
+          errorMessage: password.errorMessage,
         ),
         const SizedBox(height: 20),
         FilledButton.tonalIcon(
           onPressed: () {
-            final isValid = _formKey.currentState!.validate();
-            if (!isValid) return;
             registerCubit.onSubmit();
           },
           icon: const Icon(Icons.save),
